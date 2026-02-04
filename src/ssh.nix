846a2cc92515771e4e4b5a617ea5cf2afcb3b264
Wyatt Avilla <wyattmurphy1@gmail.com>
2026-02-03 22:42:52 -0800

    add ur ssh key

--- src/ssh.nix
+++ src/ssh.nix
@@ -1,5 +1,7 @@
 {
-  users.users.root.openssh.authorizedKeys.keys = [ "" ];
+  users.users.root.openssh.authorizedKeys.keys = [
+    "AAAAC3NzaC1lZDI1NTE5AAAAIJQBmne1OUdPfir8JpqWnt+5ELZh1hfcBsmv+SX5zxLs james@JamesPC"
+  ];
 
   services.openssh = {
     enable = true;
